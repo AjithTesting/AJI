c1c0356f2827bf6660f71703b14e6a0ed64c9066
AjithTesting <ajithajithselvam2007@gmail.com>
2023-08-17 16:41:51 +0530

    dev

--- src/org/shb/ThirdProject.java
+++ src/org/shb/ThirdProject.java
@@ -1,5 +1,13 @@
 package org.shb;
 
 public class ThirdProject {
+	
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		
+	}
 
 }
