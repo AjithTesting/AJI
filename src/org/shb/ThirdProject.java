4b3e3d5118583154e280600b6df51d16a7cef231
AjithTesting <ajithajithselvam2007@gmail.com>
2023-08-17 16:56:53 +0530

    commited

--- src/org/shb/ThirdProject.java
+++ src/org/shb/ThirdProject.java
@@ -1,5 +1,11 @@
 package org.shb;
 
 public class ThirdProject {
-
+	public static void main(String[] args) 
+	{
+	System.out.println("a");
+	System.out.println("b");
+	System.out.println("c");
+	System.out.println("d");
+	}
 }
